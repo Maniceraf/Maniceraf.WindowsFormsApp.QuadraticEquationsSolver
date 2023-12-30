b01818ac5be8f7cfd8d628742c76139b650c7d72
iamloser <hungnguyen8102000@gmail.com>
2023-12-30 16:41:00 +0700

    Add project files.

---
+++ QuadraticEquationsSolver/Main.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace QuadraticEquationsSolver
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtNghiem = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(63, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quadratic Equations Solver";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtc);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtb);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txta);
+            this.groupBox1.Location = new System.Drawing.Point(12, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter the equation coefficients:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(169, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 17);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(328, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 21);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "= 0";
+            // 
+            // txtc
+            // 
+            this.txtc.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtc.Location = new System.Drawing.Point(292, 41);
+            this.txtc.Multiline = true;
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(30, 30);
+            this.txtc.TabIndex = 7;
+            this.txtc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(265, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 21);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "+";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(240, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "X";
+            // 
+            // txtb
+            // 
+            this.txtb.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtb.Location = new System.Drawing.Point(207, 41);
+            this.txtb.Multiline = true;
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(30, 30);
+            this.txtb.TabIndex = 4;
+            this.txtb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(180, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(155, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X";
+            // 
+            // txta
+            // 
+            this.txta.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txta.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txta.Location = new System.Drawing.Point(122, 41);
+            this.txta.Multiline = true;
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(30, 30);
+            this.txta.TabIndex = 0;
+            this.txta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(13, 185);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(459, 100);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Execution:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(162, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 30);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Empty";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(318, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Solve";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtNghiem);
+            this.groupBox3.Location = new System.Drawing.Point(13, 291);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(459, 108);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "The solutions of a quadratic equation:";
+            // 
+            // txtNghiem
+            // 
+            this.txtNghiem.Enabled = false;
+            this.txtNghiem.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtNghiem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtNghiem.Location = new System.Drawing.Point(6, 22);
+            this.txtNghiem.Multiline = true;
+            this.txtNghiem.Name = "txtNghiem";
+            this.txtNghiem.Size = new System.Drawing.Size(447, 80);
+            this.txtNghiem.TabIndex = 10;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 411);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quadratic Equations Solver";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private Label label6;
+        private TextBox txtc;
+        private Label label5;
+        private Label label4;
+        private TextBox txtb;
+        private Label label3;
+        private TextBox txta;
+        private Label label7;
+        private Label label2;
+        private GroupBox groupBox2;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox3;
+        private TextBox txtNghiem;
+        private Button button3;
+    }
+}
---
+++ QuadraticEquationsSolver/Main.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuadraticEquationsSolver
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        class TAM_THUC
+        {
+            private double a; //a<>0
+            private double b;
+            private double c;
+            public double HeSoA
+            {
+                get
+                {
+                    return a;
+                }
+                set
+                {
+                    a = value;
+                }
+            }
+            public double HeSoB
+            {
+                get
+                {
+                    return b;
+                }
+                set
+                {
+                    b = value;
+                }
+            }
+            public double HeSoC
+            {
+                get
+                {
+                    return c;
+                }
+                set
+                {
+                    c = value;
+                }
+            }
+            public TAM_THUC()
+            {
+            }
+            public bool NhapHeSo(string hesoa, string hesob, string hesoc)
+            {
+                try
+                {
+                    if (hesoa.Trim().Length == 0)
+                    {
+                        MessageBox.Show("Chưa nhập hệ số a", "Thong bao loi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        return false;
+                    }
+                    else if (hesob.Trim().Length == 0)
+                    {
+                        MessageBox.Show("Chưa nhập hệ số b", "Thong bao loi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        return false;
+                    }
+                    else if (hesoc.Trim().Length == 0)
+                    {
+                        MessageBox.Show("Chưa nhập hệ số c", "Thong bao loi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        return false;
+                    }
+                    a = Double.Parse(hesoa);
+                    b = Double.Parse(hesob);
+                    c = Double.Parse(hesoc);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Loi", MessageBoxButtons.OK,
+                    MessageBoxIcon.Stop);
+                    return false;
+                }
+            }
+            public NGHIEM GiaiPT()
+            {
+                NGHIEM cNghiem = new NGHIEM();
+                double delta;
+                delta = b * b - 4 * a * c;
+                if (delta < 0)
+                    cNghiem.LoaiN = 0;
+                else if (delta == 0)
+                {
+                    cNghiem.LoaiN = 1;
+                    cNghiem.x1 = -b / 2 * a;
+                }
+                else
+                {
+                    cNghiem.LoaiN = 2;
+                    cNghiem.x1 = -b - Math.Sqrt(delta) / 2 * a;
+                    cNghiem.x2 = -b + Math.Sqrt(delta) / 2 * a;
+                }
+                return cNghiem;
+            }
+        }
+        class NGHIEM
+        {
+            private double[] x = new double[2];
+            private int LoaiNghiem;
+            public NGHIEM()
+            {
+            }
+            public int LoaiN
+            {
+                get
+                {
+                    return LoaiNghiem;
+                }
+                set
+                {
+                    LoaiNghiem = value;
+                }
+            }
+            public double x1
+            {
+                get
+                {
+                    return x[0];
+                }
+                set
+                {
+                    x[0] = value;
+                }
+            }
+            public double x2
+            {
+                get
+                {
+                    return x[1];
+                }
+                set
+                {
+                    x[1] = value;
+                }
+            }
+            public void Xuat(TextBox txtNghiem)
+            {
+                if (LoaiNghiem == 0)
+                    txtNghiem.Text = "Phương trình trên vô nghiệm.";
+                else if (LoaiNghiem == 1)
+                {
+                    txtNghiem.Text = "Phương trình trên có nghiệm kép: ";
+                    txtNghiem.Text += "x= " + x[0];
+                }
+                else
+                {
+                    txtNghiem.Text = "Phương trình trên có 2 nghiệm phân biệt: ";
+                    txtNghiem.Text += " x1 = " + x[0];
+                    txtNghiem.Text += " và x2 = " + x[1];
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TAM_THUC tt = new TAM_THUC();
+            NGHIEM N;
+
+            tt.NhapHeSo(txta.Text.Trim(), txtb.Text.Trim(), txtc.Text.Trim());
+            N = tt.GiaiPT();
+            N.Xuat(txtNghiem);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txta.Text = "";
+            txtb.Text = "";
+            txtc.Text = "";
+            txtNghiem.Text = "";
+        }
+    }
+}
